f4344171ee34573b66aa932c3424846569be3985
Tomasz Dysinski <oz@bunnybones.com>
2013-10-22 03:09:19 -0400

    grunt file to launch mongodb and beefy, though it doesn't quite work yet

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,18 +1,34 @@
 module.exports = function(grunt) {
+	function log(err, stdout, stderr, cb) {
+	    console.log(stdout);
+	    cb();
+	}
 	grunt.initConfig({
 	    shell: {
-	        multiple: {
+	        mongo: {
 	            command: [
 	                'mkdir database/db',
-	                'cd database/db',
 	                '"c:\\Program Files\\mongodb\\bin\\mongod.exe" --dbpath "c:\\Tomasz\\repos\\ToyotaBeTheHero\\sceneGrapher\\database"'
 	            ].join('&&'),
 		        options: {
-		            async: true
+		            //async: true,
+		            stdout: true,
+		            stderr: true,
+		            callback: log
 		        }
-	        }
-	    }
+	        },
+		    beefy: {
+		      command: 'beefy scripts/frontEnd.js --live --cwd client'
+		    }
+	    },
+		concurrent: {
+			dev: {
+				tasks: ['shell:mongo','shell:beefy'],
+				options: { logConcurrentOutput: true }
+			}
+		}
 	});
+	grunt.loadNpmTasks('grunt-concurrent');
 	grunt.loadNpmTasks('grunt-shell-spawn');
-	grunt.registerTask('default', ['shell']);
+	grunt.registerTask('default', ['concurrent']);
 };
